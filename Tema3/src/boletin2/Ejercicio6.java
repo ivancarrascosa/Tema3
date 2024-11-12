200711fc818155ee3656e1a638a3293e5bd14cc0
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2024-11-12 14:40:24 +0100

    Inicio boletin 3

--- Tema3/src/boletin2/Ejercicio6.java
+++ Tema3/src/boletin2/Ejercicio6.java
@@ -7,46 +7,49 @@ public class Ejercicio6 {
 	public static void main(String[] args) {
 		// Creo un array para almacenar mi apuesta
 		int miApuesta[] = new int[6];
-		
+
 		// Creo un array para almacenar el número ganador
 		int ganador[] = new int[6];
-		
-		// Creo un objeto random 
+
+		// Creo un objeto random
 		Random rand = new Random();
-		
+
 		// Creo una variable para almacenar el número random que se genere
 		int numRand;
-		
+
 		// Creo un contador
-		int cont = 0; 
-		
+		int cont = 0;
+
 		// Creo una variable para almacenar los aciertos
 		int aciertos = 0;
-		
-		// Creo un bucle while en el que se genere un número y lo introduzca en el array si el número no está ya en el array
+
+		// Creo un bucle while en el que se genere un número y lo introduzca en el array
+		// si el número no está ya en el array
 		while (cont <= 5) {
 			Arrays.sort(miApuesta);
-			numRand = rand.nextInt(1,50);
+			numRand = rand.nextInt(1, 50);
 			if (Arrays.binarySearch(miApuesta, numRand) < 0) {
-				miApuesta [0] = numRand;
+				miApuesta[0] = numRand;
 				cont++;
-			}		
+			}
 		}
-		// Reinicio el bucle y hago lo mismo que en el bucle anterior creando el array de la combinación ganadora
+		// Reinicio el bucle y hago lo mismo que en el bucle anterior creando el array
+		// de la combinación ganadora
 		cont = 0;
 		while (cont <= 5) {
 			Arrays.sort(ganador);
-			numRand = rand.nextInt(1,50);
+			numRand = rand.nextInt(1, 50);
 			if (Arrays.binarySearch(ganador, numRand) < 0) {
-				ganador [0] = numRand;
+				ganador[0] = numRand;
 				cont++;
-			}		
+			}
 		}
-		// Ordeno el ganador para poder hacer un binary search y voy buscando por cada uno de mi apuesta si coinciden
+		// Ordeno el ganador para poder hacer un binary search y voy buscando por cada
+		// uno de mi apuesta si coinciden
 		Arrays.sort(ganador);
 		for (int num : miApuesta) {
-			if (Arrays.binarySearch(ganador, num) >=0 ) {
-				aciertos+=1;
+			if (Arrays.binarySearch(ganador, num) >= 0) {
+				aciertos += 1;
 			}
 		}
 		System.out.println(Arrays.toString(miApuesta));
---
+++ Tema3/src/boletin3/Ejercicio1.java
@@ -0,0 +1,17 @@
+package boletin3;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Creamos el array que vamos a utilizar
+		int num[][] = new int[3][6];
+		// Voy insertando los números
+		num[0][0] = 0;
+		num[1][2] = 30;
+		num[1][3] = 2;
+		num[1][6] = 5;
+		num[2][0] = 75;
+
+	}
+
+}
