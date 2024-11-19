d327996f8449c50644cd56d7d6e27f805349ecb4
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2024-11-19 14:19:06 +0100

    Ejercicio 5 otra forma y correción ejercicio 5 boletin 4

--- Tema3/src/boletin4/Ejercicio5.java
+++ Tema3/src/boletin4/Ejercicio5.java
@@ -6,25 +6,29 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		int tabla[] = {1,2,3,4,1,1,2};
-		System.out.println(Arrays.toString(buscarTodos(tabla,7)));
+		System.out.println(Arrays.toString(buscarTodos(tabla,1)));
 
 	}
 	
-	public static int[] buscarTodos(int t[], int valor) {
-		// Creo una tabla que va a ser la respuesta
-		int res[];
-		
-		// Creo un contador para saber la longitud de la tabla final 
-		int cont = 0;
+	public static int apariciones(int t[], int valor) {
+		// Creo un contador para saber la longitud de la tabla final y dar la respuesta 
+		int res = 0;
 		// Recorro la tabla de entrada para saber cuantas veces aparece el valor que buscamos
 		for (int num : t) {
 			if (num == valor) {
-				cont++;
+				res++;
 			}
 		}
-		res = new int[cont];
-		// Reinicio el contador para usarlo de nuevo
-		cont = 0;
+		return res;
+	}
+	public static int[] buscarTodos(int t[], int valor) {
+		// Creo una tabla que va a ser la respuesta
+		int res[] = new int[apariciones(t,valor)];
+		
+		// Creo un contador
+		int cont = 0;
+		
+		// El for recorrera la tabla e ira metiendo los valores que coincidan en las tablas de respuesta
 		for (int i = 0; i<t.length; i++) {
 			if (t[i] == valor) {
 				res[cont] = i;
