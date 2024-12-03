cae7581d70311192b34452bd43bd1f54f8d3571a
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2024-12-03 14:41:30 +0100

    Cambios conecta 4

--- Tema3/src/boletin7/Ejercicio2.java
+++ Tema3/src/boletin7/Ejercicio2.java
@@ -3,8 +3,23 @@ package boletin7;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[][] tablero = {{"x","x","x","x"},{"-","-","-","-"}};
+		System.out.println(ganador(tablero));
 
 	}
-
+	
+	public static String ganador(String t[][]) {
+		// Creo un String para devolver el ganador
+		String res = "";
+		// Creo un for para recorrer todas las horizontales 
+		for (int i = 0 ; i <t.length ; i++) {
+			// Llego solo hasta la longtud menos 4, porque sino se me va fuera de los límites de la tabla
+			for(int j = 0 ; j <= t[i].length - 4 ; j++) {
+				if (!t[i][j].equals("-") && t[i][j].equals(t[i][j+1]) && t[i][j+1].equals(t[i][j+2]) && t[i][j+2].equals(t[i][j+3])) {
+					res = t[i][j];
+				}
+			}
+		}
+		return res;
+	}
 }
